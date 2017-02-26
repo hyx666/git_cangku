9867cd4ada7e4fcd474e0346cb93f63599aa2f4c
qiweiwei <1063188362@qq.com>
2017-02-26 17:16:41 +0800

    华堆的哈哈哈哈

---
+++ src/main/java/test1/aaa/haha.java
@@ -0,0 +1,12 @@
+package test1.aaa;
+
+/**
+ * Created by Administrator on 2017-02-26.
+ */
+public class haha {
+    public static void main(String[] args){
+        System.out.println("try again and again");
+        System.out.println("try again and again and again");
+        System.out.println("ohohoh");
+    }
+}

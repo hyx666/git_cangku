2b19bcea3b7ec3efc73c1e095740cf40d35cc235
huayang <764220199@qq.com>
2017-02-26 15:45:31 +0800

    第一个版本

---
+++ src/main/java/test1/aaa/demo1.java
@@ -0,0 +1,10 @@
+package test1.aaa;
+
+/**
+ * Created by Administrator on 2017-02-26.
+ */
+public class demo1 {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}

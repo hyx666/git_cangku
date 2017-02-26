9a347a45f651dcc3b4dd1e05395c4de5ab74fdae
huayang <764220199@qq.com>
2017-02-26 15:52:47 +0800

    第二个版本

---
+++ src/main/java/test1/aaa/demo2.java
@@ -0,0 +1,11 @@
+package test1.aaa;
+
+/**
+ * Created by Administrator on 2017-02-26.
+ */
+public class demo2 {
+    public static void main(String[] args){
+        System.out.println("This is the second Version");
+        System.out.println("Hello,hyx!");
+    }
+}
